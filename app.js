d1ae04957c06b40ad4dc7fc740e32d9711e09240
Zidan Harb <zidan.harb2012@gmail.com>
2025-01-05 14:36:51 -0600

    /

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public\static'));
 
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/some-endpoint', (req, res) => {
-    const data = req.body.data;
-    res.json({ received: data });
+app.get('/', (req, res) => {
+    res.sendFile(path.join('/home/zidan/Zidan/login', 'index.html'));
 });
 
 app.listen(8000, () => {
