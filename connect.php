a065aabe4589a6a4a4e027a1c82279bf8343e41e
Zidan Harb <zidan.harb2012@gmail.com>
2025-01-12 12:47:34 -0600

    /

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?php
 
 $host = "localhost";
+$port = 81;
 $user = "root";
 $pass = "zidan123";
 $db = "Zarcotech_Users";
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $port, $user, $pass, $db);
 
 if ($conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
